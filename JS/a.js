0cc8afd85f1e4fa195d28d2f0a4398fc3a22cc2b
meneace <j.mendez@outlook.com>
2018-04-27 19:45:49 -0500

    maps

--- JS/a.js
+++ JS/a.js
@@ -1,6 +1,6 @@
-let j = require('./b.js');
-let k = require('./c.js');
-let l = require('./d.js');
+let mult = require("./mult.js");
+//let k = require('./c.js');
+//let l = require('./d.js');
 
 /*function a() {
     console.log('Funcion a');
@@ -8,13 +8,17 @@ let l = require('./d.js');
     return a;
 }
 
-a();*/
-
-function b(j, k) {
-    let a = 5;
-    let b = 6;
-    l(a, b);
-    k(a, b);
-
+a();
+*/
+function b() {
+    let a = 9,
+        b = 2;
+    console.log('Funcion Madre.\nlos valores a sumar,mult y restar son:\na:' + a + '   b: ' + b + '\n');
+    // Llamamos las funciones importadas y dependiendo de lo que haga cada funcion en su respectivo archivo es como lo implementaremos.
+    // En este ejemplo la funcion suma, retorna valores; es entonces que debemos imprimir ese valor;
+    // console.log('Funcion suma: ', suma(a,b));
+    // En este caso, la funcion resta que vive dentro de rest.js no retorna nada, solo recibe dos variables y dentro de la funcion imprime la resta de esos dos valores.
+    // resta(a,b);
+    console.log('Funcion mult: ', mult(a, b));
 }
 b();
---
+++ JS/mult.js
@@ -0,0 +1,13 @@
+/*module.exports = function(j, k) {
+    var x = 3;
+    var y = 2;
+    j.value = x;
+    k.value = y;
+
+}*/
+
+function mult(a, b) {
+    return a * b;
+}
+
+module.exports = mult;
