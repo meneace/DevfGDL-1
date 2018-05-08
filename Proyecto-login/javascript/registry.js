3d9bdd0d3a1a1804c81d93e2ea50cb30bd16cedd
meneace <j.mendez@outlook.com>
2018-05-08 00:05:01 -0500

    JS botones y barra progresion

--- Proyecto-login/javascript/registry.js
+++ Proyecto-login/javascript/registry.js
@@ -1,78 +1,55 @@
-//jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
+var activo_FS, previo_FS, siguiente_FS, primero_FS;
 
-$(".next").click(function() {
-    if (animating) return false;
-    animating = true;
+$(document).ready(function() {
+    /**Boton siguiente de la primer pantalla.
+     * Oculta la primer pantalla, muestra la segunda 
+     * y avanza la barra de progresion 
+     */
+    $('.siguiente').click(function() {
+        activo_FS = $('#uno');
+        siguiente_FS = $('#dos');
+        $("#avance li").eq($("fieldset").index(siguiente_FS)).addClass("active");
+        siguiente_FS.show();
+        activo_FS.hide();
 
-    current_fs = $(this).parent();
-    next_fs = $(this).parent().next();
+        /** Boton previo de la segunda pantalla.
+         * Oculta la segunda pantalla y muestra la primera,
+         * atrasa una posicion la barra de progresion.
+         */
+        $('.previo').click(function() {
+            activo_FS = $('#dos');
+            previo_FS = $('#uno');
+            $("#avance li").eq($("fieldset").index(activo_FS)).removeClass("active");
+            previo_FS.show();
+            activo_FS.hide();
+        });
+        /**Boton siguiente de la segunda pantalla.
+         * Oculta la segunda pantalla y muestra la tercera,
+         * recorre una posicion la barra de progresion.
+         */
+        $('.sigui').click(function() {
+            activo_FS = $('#dos');
+            siguiente_FS = $('#tres');
+            $("#avance li").eq($("fieldset").index(siguiente_FS)).addClass("active");
+            siguiente_FS.show();
+            activo_FS.hide();
+        });
 
-    //activate next step on progressbar using the index of next_fs
-    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+        /**Boton previo de la tercera pantalla.
+         * Oculta la tercer pantalla y muestra la segunda,
+         * atrasa una posicion la barra de progesion.
+         */
 
-    //show the next fieldset
-    next_fs.show();
-    //hide the current fieldset with style
-    current_fs.animate({ opacity: 0 }, {
-        step: function(now, mx) {
-            //as the opacity of current_fs reduces to 0 - stored in "now"
-            //1. scale current_fs down to 80%
-            scale = 1 - (1 - now) * 0.2;
-            //2. bring next_fs from the right(50%)
-            left = (now * 50) + "%";
-            //3. increase opacity of next_fs to 1 as it moves in
-            opacity = 1 - now;
-            current_fs.css({
-                'transform': 'scale(' + scale + ')',
-                'position': 'absolute'
-            });
-            next_fs.css({ 'left': left, 'opacity': opacity });
-        },
-        duration: 800,
-        complete: function() {
-            current_fs.hide();
-            animating = false;
-        },
-        //this comes from the custom easing plugin
-        easing: 'easeInOutBack'
-    });
-});
+        $('#previo').click(function() {
+            activo_FS = $('#tres');
+            previo_FS = $('#dos');
+            primero_FS = $('#uno');
+            $("#avance li").eq($("fieldset").index(activo_FS)).removeClass("active");
+            previo_FS.show();
+            activo_FS.hide();
+            primero_FS.hide();
 
-$(".previous").click(function() {
-    if (animating) return false;
-    animating = true;
-
-    current_fs = $(this).parent();
-    previous_fs = $(this).parent().prev();
-
-    //de-activate current step on progressbar
-    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-    //show the previous fieldset
-    previous_fs.show();
-    //hide the current fieldset with style
-    current_fs.animate({ opacity: 0 }, {
-        step: function(now, mx) {
-            //as the opacity of current_fs reduces to 0 - stored in "now"
-            //1. scale previous_fs from 80% to 100%
-            scale = 0.8 + (1 - now) * 0.2;
-            //2. take current_fs to the right(50%) - from 0%
-            left = ((1 - now) * 50) + "%";
-            //3. increase opacity of previous_fs to 1 as it moves in
-            opacity = 1 - now;
-            current_fs.css({ 'left': left });
-            previous_fs.css({ 'transform': 'scale(' + scale + ')', 'opacity': opacity });
-        },
-        duration: 800,
-        complete: function() {
-            current_fs.hide();
-            animating = false;
-        },
-        //this comes from the custom easing plugin
-        easing: 'easeInOutBack'
+        });
     });
 });
 
